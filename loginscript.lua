44fbd26e1b091116f36613a85be70630a530720b
ConsolidateZ <129752480+ConsolidateZ@users.noreply.github.com>
2023-04-03 18:22:11 +0200

    Update loginscript.lua

--- loginscript.lua
+++ loginscript.lua
@@ -15,29 +15,10 @@ local waypoints = {
 local pathid = AddTaxiPath(waypoints, 31124, 31124) -- these mountIDs are NPC entries
 
 local function FirstLogin(event, player)
-    player:SendNotification("|cFFffc100Welcome to AmdWoW")
 	player:SendBroadcastMessage("|cFFffc100Welcome to AmdWoW")
     player:StartTaxi(pathid)
-    player:RegisterEvent(messagetwo, 3000, 1)
 end
 
-function messagetwo(eventId, time, repeats, player)
-    player:SendNotification("|cFFffc100We hope you will enjoy the server, and its content. Starting gear is to your left.")
-	player:SendBroadcastMessage("|cFFffc100We hope you will enjoy the server, and its content. Starting gear is to your left.")
-    player:RegisterEvent(messagethree, 3000, 1)
-end
-
-function messagethree(eventId, time, repeats, player)
-    player:SendNotification("|cFFffc100If you need any help, use our world chat /1 or make a ticket.")
-	player:SendBroadcastMessage("|cFFffc100If you need any help, use our world chat /1 or make a ticket.")
-    player:RegisterEvent(messagefour, 3000, 1)
-end
-
-function messagefour(eventId, time, repeats, player)
-    player:SendNotification("|cFFffc100Sincerly AmdWoW")
-	player:SendBroadcastMessage("|cFFffc100Sincerly AmdWoW")
-	player:SetBindPoint(player:GetX(), player:GetY(), player:GetZ(), player:GetMapId(), player:GetAreaId())
-end
 
 RegisterPlayerEvent(30, FirstLogin)
 
